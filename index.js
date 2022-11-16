f397f183efd966a4ad0269b60f7a08d0e7feb289
Jose Campos <Jose_cmps14@yahoo.com>
2022-11-15 22:18:33 -0600

    commit tree 24373444dd4e73075db5c72e0c578dab7e4f7b13

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+// function superbowlWin(record) {
+//     const yearObj= record.find(obj => obj.result === 'W') 
+//     console.log (yearObj)
+//     if (yearObj === undefined) {
+//         return undefined 
+//     }
+//     return yearObj.year 
+// }
+
+function superbowlWin(array){
+    for (const object of array)
+    if (object.result === "W"){
+        return object.year
+    }
+}
+
+record.find(superbowlWin)
